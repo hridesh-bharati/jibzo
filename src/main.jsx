0be8caeeea5e4e02d8afce803cc3508ccae5a197
Hridesh Bharati <hridesh027@gmail.com>
2025-09-13 03:09:19 +0530

    whatspp like U2

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,10 @@ root.render(
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('SW registered', reg))
+      .then(reg => {
+        console.log('SW registered', reg);
+      })
       .catch(err => console.log('SW registration failed', err));
   });
 }
+
