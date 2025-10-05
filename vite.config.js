0d161cdc2d2c173fb2bbc95b94729d94148222e3
Hridesh Bharati <hridesh027@gmail.com>
2025-10-05 15:51:34 +0530

    vite + vercel neew

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
         scope: "/",
         icons: [
           {
-            src: "icons/logo-192.png",
+            src: "/icons/logo-192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "icons/logo-512.png", 
+            src: "/icons/logo-512.png", 
             sizes: "512x512",
             type: "image/png"
           }
@@ -50,5 +50,6 @@ export default defineConfig({
       }
     }
   },
-  base: "/" 
+  base: "./", // ✅ Yeh change karein
+  publicDir: "public" // ✅ Explicitly set karein
 });
